b8810a1f15150f30bd58ea207e95ce32ac14ffc7
FabioFarinella96 <fabiofarinella96@gmail.com>
2023-04-04 20:04:18 +0200

    twitter-clone

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -1,14 +1,14 @@
 import "./index.css";
-// import { TbMessages } from "react-icons/Tb";
-// import { MdNotificationsActive } from "react-icons/Md";
-// import { FaHome, FaHashtag } from "react-icons/Fa";
-// import {
-//   BsBookmarkStar,
-//   BsCardList,
-//   BsMoonStarsFill,
-//   BsSun,
-// } from "react-icons/Bs";
-// import { CgMoreO } from "react-icons/Cg";
+import { TbMessages } from "react-icons/Tb";
+import { MdNotificationsActive } from "react-icons/Md";
+import { FaHome, FaHashtag } from "react-icons/Fa";
+import {
+  BsBookmarkStar,
+  BsCardList,
+  BsMoonStarsFill,
+  BsSun,
+} from "react-icons/Bs";
+import { CgMoreO } from "react-icons/Cg";
 
 const SideMenu = ({ darkMode, setDarkMode }) => {
   return (
@@ -19,27 +19,39 @@ const SideMenu = ({ darkMode, setDarkMode }) => {
         alt="twitter-icon"
       />
       <div className="home">
-        <div className="explore-icon">{/* <FaHome /> */}</div>
+        <div className="explore-icon">
+          <FaHome />
+        </div>
         <p className="home-text">Home</p>
       </div>
       <div className="explore">
-        <div className="explore-icon">{/* <FaHashtag /> */}</div>
+        <div className="explore-icon">
+          <FaHashtag />
+        </div>
         <p className="explore-text">Explore</p>
       </div>
       <div className="notification">
-        <div className="explore-icon">{/* <MdNotificationsActive /> */}</div>
+        <div className="explore-icon">
+          <MdNotificationsActive />
+        </div>
         <p className="notification-text">Notification</p>
       </div>
       <div className="messages">
-        <div className="explore-icon">{/* <TbMessages /> */}</div>
+        <div className="explore-icon">
+          <TbMessages />
+        </div>
         <p className="messages-text">Messages</p>
       </div>
       <div className="messages">
-        <div className="explore-icon">{/* <BsBookmarkStar /> */}</div>
+        <div className="explore-icon">
+          <BsBookmarkStar />
+        </div>
         <p className="messages-text">Bookmarks</p>
       </div>
       <div className="messages">
-        <div className="explore-icon">{/* <BsCardList /> */}</div>
+        <div className="explore-icon">
+          <BsCardList />
+        </div>
         <p className="messages-text">Lists</p>
       </div>
       <div className="messages">
@@ -49,12 +61,12 @@ const SideMenu = ({ darkMode, setDarkMode }) => {
         >
           {darkMode ? (
             <p className="sun">
-              {/* <BsSun className="sun" /> */}
+              <BsSun className="sun" />
               Light mode
             </p>
           ) : (
             <p className="moon">
-              {/* <BsMoonStarsFill className="moon" /> */}
+              <BsMoonStarsFill className="moon" />
               Dark mode
             </p>
           )}
@@ -67,7 +79,9 @@ const SideMenu = ({ darkMode, setDarkMode }) => {
         <p className="messages-text"> Profile</p>
       </div>
       <div className="messages">
-        <div className="explore-icon">{/* <CgMoreO /> */}</div>
+        <div className="explore-icon">
+          <CgMoreO />
+        </div>
         <p className="messages-text">More</p>
       </div>
       <div className="button">
